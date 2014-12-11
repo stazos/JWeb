8f856b42954526d3c53976ea5d7dfc2a9a56618a
Mathieu VESY <mathieu.vesy@gmail.com>
2014-12-11 17:10:27 +0100

    commit user + d'autre truc

--- JWeb/src/model/User.java
+++ JWeb/src/model/User.java
@@ -1,5 +1,9 @@
 package model;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.Date;
 
 public class User {
@@ -11,6 +15,30 @@ public class User {
 	private Boolean admin;
 	
 	public String getFirstName() {
+		/* Connexion à la base de données */
+		String url = "jdbc:mysql://localhost:3306/bdd_sdzee";
+		String utilisateur = "java";
+		String motDePasse = "$dZ_£E";
+		Connection connexion = null;
+		try {
+			connexion = DriverManager.getConnection(url, utilisateur,
+					motDePasse);
+			/* Création de l'objet gérant les requêtes */
+			Statement statement = connexion.createStatement();
+			/* Ici, nous placerons nos requêtes vers la BDD */
+			/* ... */
+
+		} catch (SQLException e) {
+			/* Gérer les éventuelles erreurs ici */
+		} finally {
+			if (connexion != null)
+				try {
+					/* Fermeture de la connexion */
+					connexion.close();
+				} catch (SQLException ignore) {
+					/* Si une erreur survient lors de la fermeture, il suffit de l'ignorer. */
+				}
+		}
 		return firstName;
 	}
 	public void setFirstName(String firstName) {
