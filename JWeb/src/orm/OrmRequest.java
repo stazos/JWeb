ab002987fe1286047cda55931a85274ef4b1c6bc
Mathieu VESY <mathieu.vesy@gmail.com>
2015-01-07 10:03:50 +0100

    documentation orm

--- JWeb/src/orm/OrmRequest.java
+++ JWeb/src/orm/OrmRequest.java
@@ -16,6 +16,12 @@ public class OrmRequest {
 		request = "";
 	}
 
+	/**
+	 * creation du debut d'une requette select, Args sont les colones qui vont
+	 * etre selectionner.
+	 * 
+	 * @param Args
+	 */
 	public void Select(String... Args) {
 		int i = 0;
 		request = "SELECT ";
@@ -27,6 +33,11 @@ public class OrmRequest {
 		}
 	}
 
+	/**
+	 * ajout de from a la requete, Args sont les tables selectionner
+	 * 
+	 * @param Args
+	 */
 	public void From(String... Args) {
 		int i = 0;
 		request += " FROM ";
@@ -38,6 +49,12 @@ public class OrmRequest {
 		}
 	}
 
+	/**
+	 * ajout de where a la requete, map la requete va etre creer comme key =
+	 * value AND key = value
+	 * 
+	 * @param map
+	 */
 	public void Where(HashMap<String, Object> map) {
 		int i = 0;
 		request += " WHERE ";
@@ -56,10 +73,23 @@ public class OrmRequest {
 		}
 	}
 
+	/**
+	 * commence une requete delete, nameTable est le nom de la table sur laquel
+	 * on veut faire des delete
+	 * 
+	 * @param nameTable
+	 */
 	public void DeleteFrom(String nameTable) {
 		request = "DELETE FROM " + nameTable;
 	}
 
+	/**
+	 * cree une requete insert into, nameTable est le nom de la table et Args
+	 * les value que lon veut inserer
+	 * 
+	 * @param nameTable
+	 * @param Args
+	 */
 	public void InsertInto(String nameTable, Object... Args) {
 		request = "INSERT INTO " + nameTable;
 		request += " VALUE (";
@@ -76,11 +106,24 @@ public class OrmRequest {
 		request += ")";
 	}
 
+	/**
+	 * cree une requete update nom avec le nameTable, qui creer setName =
+	 * setValue
+	 * 
+	 * @param nameTable
+	 * @param setName
+	 * @param setValue
+	 */
 	public void Update(String nameTable, String setName, String setValue) {
 		request = "UPDATE " + nameTable;
 		request += " SET " + setName + " = " + setValue;
 	}
 
+	/**
+	 * execute un query de selection
+	 * 
+	 * @return
+	 */
 	public ResultSet ExecuteQuery() {
 		request += ";";
 		System.out.println(request);
@@ -94,6 +137,11 @@ public class OrmRequest {
 		return resultat;
 	}
 
+	/**
+	 * execute un query de mise a jour de la base de donne
+	 * 
+	 * @return
+	 */
 	public int ExecuteUpdate() {
 		request += ";";
 		System.out.println(request);
